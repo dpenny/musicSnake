dc6b4aa41c2d16c576011c774361be54a46fcf4f
Danielle Penny <dpenny@mit.edu>
2016-04-24 18:16:00 -0400

    playing notes at once

--- js/snake.js
+++ js/snake.js
@@ -13,20 +13,59 @@ $(document).ready(function(){
 	var musicSnake=[]; //array of notes in the snake
 	var availableNotes=[]; //array of notes/food currently on the canvas
     var colorToNote=[{color:"#CE93D8",note:"C"}, {color:"#01579B",note:"G"},{color:"#FFD45F",hasOwnPropertye:"R"}]//color note tuple
-    
+    var scale=[53,55,57,58,60,62,64,65]; //f major scale
     function init(){
         direction="right";
 		make_snake();
         make_note();
 		
-		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		
 		game_loop = setInterval(paint, 60);
+        
+        song_loop = setInterval(playSnake, 1000);
     }
 	
 	   
-        init();
+    MIDI.loadPlugin({
+		soundfontUrl: "./soundfont2/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			//console.log(state, progress);
+		},
+		onsuccess: function() {
+            midiLoaded=true;
+            init();
+            
+		}
+	});
     
-    function playNote(){
+    //plays the snake built so far
+    function playSnake(){
+            var snake=musicSnake;
+        console.log(snake);
+        var duration=1.5;
+            var delay = 0; 
+			var velocity = 127;
+			MIDI.setVolume(0, 127);
+        for (var i=0; i<snake.length; i++){
+            console.log(snake[i].note);
+            MIDI.noteOn(0, snake[i].note, velocity, delay);
+			MIDI.noteOff(0, snake[i].note, delay +duration);
+            
+        }
+			
+        
+        
+    }
+    //plays a note when it's first eaten
+    function playNote(note){
+        var delay = 0; // play one note every quarter second
+			//var note = food.note; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
         
     }
 	 
@@ -51,10 +90,8 @@ $(document).ready(function(){
     
     //making the snake, initialized with a length of 6
 	function make_snake(){		
-		for(var i =5; i>=0; i--){
-            var rand = colorToNote[Math.floor(Math.random() * colorToNote.length)]
-            console.log(rand, "rand");
-			musicSnake.push({x: i, y:3, color:rand.color, note:rand.note});
+		for(var i =2; i>=0; i--){          
+			musicSnake.push({x: i, y:3, color:"red", note:scale[Math.floor(Math.random() * scale.length)]});
 		}
 	}
 	
@@ -62,7 +99,7 @@ $(document).ready(function(){
     //makes a single random note
 	function make_note(){
         var rand = colorToNote[Math.floor(Math.random() * colorToNote.length)];
-        var pitch=Math.floor(Math.random() * (72 - 50 + 1)) + 50;
+        var pitch=scale[Math.floor(Math.random() * scale.length)]
 
 		food = {
 			x: Math.round(Math.random()*(width-cellSize)/cellSize), 
@@ -70,14 +107,13 @@ $(document).ready(function(){
             color:rand[0],
             note:pitch,
 		};
-        console.log(food);
+        //console.log(food);
         
         return food;
 	}
 	var headColor;
 	function paint(){
         //canvas
-        //console.log(midiLoaded);
 		ctx.fillStyle = "white";
 		ctx.fillRect(0, 0, width, height);
 		ctx.strokeStyle = "black";
@@ -115,6 +151,8 @@ $(document).ready(function(){
 			var tail = musicSnake.pop(); //pops out the last cell
 			tail.x = headX;
             tail.y = headY;
+            tail.color="red";
+            tail.note=scale[Math.floor(Math.random() * scale.length)];
 		}
 		musicSnake.unshift(tail);
         
@@ -158,3 +196,4 @@ $(document).ready(function(){
 	
 	
 })
+
